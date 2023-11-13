0ede48c2b5cbe86b812061e6d5ab74016bbbe436
caiogmello <caiogomedemello@gmail.com>
2023-11-13 14:23:34 -0300

    FIFO praticamente finalizado, porém com um erro descrito no TODO

--- Memory.py
+++ Memory.py
@@ -29,14 +29,46 @@ class Memory:
         return pages
         
     
-    def reallocateInDisk(self):
-        return
+    def reallocateInDisk(self, process:Processo):
+        i = self.memory.index(process.getId())
+        last_page = i + process.getPaginas()
+        
+        while (i < last_page):
+            self.memory[i] = "-"
+            i+=1
+        
+        start = 0
+        end = 0
+        for i in range(self.diskSize):
+            if (end - start) == process.getPaginas():
+                break
+            
+            if self.disk[i] == "-":
+                end += 1
+            else:
+                start = i
+                end = i
+        
+        while (start < end):
+            self.disk[start] = process.getId()
+            start += 1
+        
+    def removeFromDisk(self, process:Processo):
+        i = self.disk.index(process.getId())
+        last_page = i + process.getPaginas()
+        
+        while (i < last_page):
+            self.disk[i] = "-"
+            i+=1
     
     def allocateInMemory(self, process:Processo):
         if process.getId() in self.memory:
             return
-           
-        if self.freeSpace > process.getPaginas():
+              
+        self.removeFromDisk(process=process)
+        
+        if self.freeSpace >= process.getPaginas():
+            # TODO: mudar essa lógica, pois dá erro quando tem espaço suficiente e o espaço não é contínuo, mas sim fragmentado entre a memória
             i = self.memory.index("-")
             last_page = i + process.getPaginas()
             while(i<last_page):
@@ -45,9 +77,34 @@ class Memory:
             
             self.freeSpace -= process.getPaginas()
             self.queue.append(process)
-            print(self.queue[0].getId())
-        
+
+        else:
+            if self.algorithm == "FIFO":
+                space_to_be_realocated = self.queue[0].getPaginas()
+                                
+                while(space_to_be_realocated < process.getPaginas()):
+                    self.reallocateInDisk(process=self.queue[0])
+                    self.queue.popleft()
+                    
+                    space_to_be_realocated += self.queue[0].getPaginas()
+                    
+                self.freeSpace += space_to_be_realocated
+                self.reallocateInDisk(process=self.queue[0])
+                    
+
+                i = self.memory.index("-")
+                last_page = i + process.getPaginas()
                 
+                while(i<last_page):
+                    self.memory[i] = process.getId()
+                    i+=1
+                
+                
+                self.freeSpace -= process.getPaginas()
+                self.queue.append(process)
+        
+                    
+                           
     
     def getMemory(self):
         return self.memory
@@ -57,10 +114,10 @@ class Memory:
 
 
 
-p1 = Processo(0, 2, 3, 5, 1, 5)
-p2 = Processo(1, 3, 5, 7, 2, 7)
-p3 = Processo(2, 4, 6, 2, 3, 1)
-p4 = Processo(3, 2, 2, 8, 4, 2)
+p1 = Processo(0, 2, 3, 5, 1, 12)
+p2 = Processo(1, 3, 5, 7, 2, 12)
+p3 = Processo(2, 4, 6, 2, 3, 12)
+p4 = Processo(3, 2, 2, 8, 4, 15)
     
 all_processes_example = {
     p1: p1.getPaginas(),
@@ -71,16 +128,15 @@ all_processes_example = {
 
 a = Memory("FIFO", all_processes_example)
 
+print(a.getDisk())
 a.allocateInMemory(p1)
 a.allocateInMemory(p2)
 a.allocateInMemory(p3)
+print(a.getMemory())
 a.allocateInMemory(p4)
-
+print(a.getMemory())
+print(a.getDisk())
+a.allocateInMemory(p1)
+print(a.getMemory())
 print(a.getDisk())
 
-print(a.getMemory())
-
-
-a.allocateInMemory(p2)
-
-print(a.getMemory())
